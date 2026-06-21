0c001c13129aad2d8dba32804d3818e5cefbe1b9
mattiadigrazia2008-cpu <mattiadigrazia2008@gmail.com>
2026-06-21 21:15:41 +0200

    Add files via upload

---
+++ www/sw.js
@@ -0,0 +1,30 @@
+const CACHE = "guess-the-time-v1";
+const ASSETS = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return (
+        cached ||
+        fetch(event.request)
+          .then((res) => {
+            const copy = res.clone();
+            caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+            return res;
+          })
+          .catch(() => cached)
+      );
+    })
+  );
+});
